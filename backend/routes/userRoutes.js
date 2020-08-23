97bb155f97f38a90ae55b72e3fc1f57470bf874c
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-23 10:21:11 +0530

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,71 @@
+import express, { response } from "express";
+import User from "../models/userModel";
+import { getToken, isAdmin, isAuth } from "../util";
+
+const router = express.Router();
+
+router.post("/signin", async (req, res) => {
+  try {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log(getToken(signinUser));
+    if (signinUser) {
+      res.send({
+        _id: signinUser._id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: getToken(signinUser),
+      });
+    } else {
+      res.status(401).send({ message: "Invalid Email or password" });
+    }
+  } catch (error) {
+    res.status(401).send({ message: error.message });
+  }
+});
+
+router.get("/createAdmin", async (req, res) => {
+  try {
+    const user = new User({
+      name: "Sahil",
+      email: "sahil.dev@gmail.com",
+      password: "123456789",
+      isAdmin: true,
+    });
+
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = await User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const newUser = await user.save();
+
+    if (newUser) {
+      res.send({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: getToken(newUser),
+      });
+    } else {
+      res.status(401).send({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    res.status(401).send({ message: error.message });
+  }
+});
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoutes";
+import productRoute from "./routes/productRoutes";
+import orderRoute from "./routes/orderRoute";
+import uploadRoute from "./routes/uploadRoute";
+import bodyParser from "body-parser";
+
+dotenv.config({ path: `${__dirname}/config.env` });
+const app = express();
+
+app.use(bodyParser.json());
+
+//const mongodbUrl = config.MONGODB_URL;
+const mongodbUrl = process.env.MONGODB_ATLAS;
+
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error.message));
+
+app.use("/api/uploads", uploadRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
+app.use(express.static(path.join(__dirname, "/../flipdeal/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../flipdeal/build/index.html`));
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
+});

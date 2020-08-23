45e634a7ac1ee8e0955be12ac84844414593b8bf
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-23 20:03:01 +0530

    new commit-20

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../flipdeal/build/index.html`));
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);

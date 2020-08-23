664bf9b7f98baff1a7e5fe1c6b2e04f2a7ccc807
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-23 20:53:02 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,13 @@ app.get("/api/config/paypal", (req, res) => {
 });
 app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/../flipdeal/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/../flipdeal/build/index.html`));
-  });
-}
+app.use(express.static(path.join(__dirname, "/../flipdeal/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "flipdeal", "build", "index.html"));
+});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/../flipdeal/build/index.html`));
+// });
 
 const PORT = process.env.PORT || 8000;
 

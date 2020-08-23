233e47169bfeba6367532c77e3dbf961a6729ebf
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-23 22:56:25 +0530

    new commit-20

--- backend/server.js
+++ backend/server.js
@@ -34,14 +34,18 @@ app.get("/api/config/paypal", (req, res) => {
 });
 app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 
-// app.use(express.static("flipdeal/build"));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "flipdeal", "build", "index.html"));
 // });
-app.use(express.static(path.join(__dirname, "/../../flipdeal/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../../flipdeal/build/index.html`));
-});
+//app.use(express.static(path.join(__dirname, "/../flipdeal/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/../flipdeal/build/index.html`));
+// });
+app.use(express.static("flipdeal/build"));
+app.use(
+  "*",
+  express.static(path.join(__dirname, "flipdeal", "build", "index.html"))
+);
 
 const PORT = process.env.PORT || 8000;
 
